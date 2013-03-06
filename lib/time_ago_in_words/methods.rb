10395c6741efab7c0b3cc9391ffcc44b2338d398
Leo Gallucci <elgalu3@gmail.com>
2013-03-06 10:44:27 -0300

    Applied Extract Method refactor on Time#ago_in_words()

--- lib/time_ago_in_words/methods.rb
+++ lib/time_ago_in_words/methods.rb
@@ -24,18 +24,30 @@ module TimeAgoInWords
     return 'just now' if secs > -1 && secs < 1
     return '' if secs <= -1
     return 'a very very long time ago' if secs > 60*60*24*100_000
-    ary = [[60, :seconds], [60, :minutes], [24, :hours], [100_000, :days]].map{ |count, name|
+    pair = ago_in_words_pair(secs)
+    ary = ago_in_words_singularize(pair)
+    ary.size == 0 ? '' : ary.join(' and ') << ' ago'
+  end
+
+  private
+
+  # @private
+  def ago_in_words_pair(secs)
+    [[60, :seconds], [60, :minutes], [24, :hours], [100_000, :days]].map{ |count, name|
       if secs > 0
         secs, n = secs.divmod(count)
         "#{n.to_i} #{name}"
       end
     }.compact.reverse[0..1]
-    if ary.size == 1
-      ary.map! {|part| part[0, 2].to_i == 1 ? part.chomp('s') : part }
+  end
+
+  # @private
+  def ago_in_words_singularize(pair)
+    if pair.size == 1
+      pair.map! {|part| part[0, 2].to_i == 1 ? part.chomp('s') : part }
     else
-      ary.map! {|part| part[0, 2].to_i == 1 ? part.chomp('s') : part[0, 2].to_i == 0 ? nil : part }
+      pair.map! {|part| part[0, 2].to_i == 1 ? part.chomp('s') : part[0, 2].to_i == 0 ? nil : part }
     end
-    ary.compact!
-    ary.size == 0 ? '' : ary.join(' and ') << ' ago'
+    pair.compact
   end
 end
