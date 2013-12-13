f82c78b5be7ca3eb67be1af1e5a2941ef46ff9da
Leo Gallucci <elgalu3@gmail.com>
2013-12-12 21:38:58 -0300

    Compy with new improved RSpec expect() syntax. Upgrade Gems

--- spec/time_ago_in_words_spec.rb
+++ spec/time_ago_in_words_spec.rb
@@ -2,53 +2,55 @@ require 'spec_helper'
 
 describe TimeAgoInWords do
   it 'should have a version number' do
-    TimeAgoInWords::VERSION.should_not be_nil
+    expect(TimeAgoInWords::VERSION).not_to be_nil
   end
 
   describe '#ago_in_words' do
-    context 'invalid dates' do
-      it "returns 'just now' when elapsed seconds are less than 1" do
-        Time.now.ago_in_words.should == 'just now'
-        (Time.now - 0.5).ago_in_words.should == 'just now'
-        (Time.now + 0.5).ago_in_words.should == 'just now'
-      end
-
-      it "returns empty string on future dates" do
-        (Time.now + 1.6).ago_in_words.should == ''
-        (Time.now + 100).ago_in_words.should == ''
-        (Time.now + 9999).ago_in_words.should == ''
+    context 'unsupported dates' do
+      it "returns empty string in future dates" do
+        expect((Time.now + 1.6).ago_in_words).to eq('')
+        expect((Time.now + 100).ago_in_words).to eq('')
+        expect((Time.now + 9999).ago_in_words).to eq('')
       end
 
       it "returns 'a very very long time ago' when more than 300 years ago" do
-        Time.local(1713,01,01).ago_in_words.should == 'a very very long time ago'
+        expect(Time.local(1713,01,01).ago_in_words).to eq('a very very long time ago')
+      end
+    end
+
+    context 'valid dates' do
+      it "returns 'just now' when elapsed seconds are less than 1" do
+        expect(Time.now.ago_in_words).to eq('just now')
+        expect((Time.now - 0.5).ago_in_words).to eq('just now')
+        expect((Time.now + 0.5).ago_in_words).to eq('just now')
       end
 
       it "can handle many days ago" do
-        (Time.now - 60*60*24*9999).ago_in_words.should == '9999 days ago'
+        expect((Time.now - 60*60*24*9999).ago_in_words).to eq('9999 days ago')
       end
 
       it "returns N seconds ago, N seconds ago!" do
-        (Time.now - 2).ago_in_words.should == '2 seconds ago'
-        (Time.now - 33).ago_in_words.should == '33 seconds ago'
+        expect((Time.now - 2).ago_in_words).to eq('2 seconds ago')
+        expect((Time.now - 33).ago_in_words).to eq('33 seconds ago')
       end
 
       it "returns 1 second/minute/hour/day ago when 1 ago!" do
-        (Time.now - 1).ago_in_words.should        == '1 second ago'
-        (Time.now - 60).ago_in_words.should       == '1 minute ago'
-        (Time.now - 60*60).ago_in_words.should    == '1 hour ago'
-        (Time.now - 60*60*24).ago_in_words.should == '1 day ago'
+        expect((Time.now - 1).ago_in_words).to eq('1 second ago')
+        expect((Time.now - 60).ago_in_words).to eq('1 minute ago')
+        expect((Time.now - 60*60).ago_in_words).to eq('1 hour ago')
+        expect((Time.now - 60*60*24).ago_in_words).to eq('1 day ago')
       end
 
       it "returns 1 second/minute/hour/day and 1 second/minute/hour/day ago when appropriate" do
-        (Time.now - 1 - 60).ago_in_words.should             == '1 minute and 1 second ago'
-        (Time.now - 1*60*60 - 60).ago_in_words.should       == '1 hour and 1 minute ago'
-        (Time.now - 1*60*60*24 - 60*60).ago_in_words.should == '1 day and 1 hour ago'
+        expect((Time.now - 1 - 60).ago_in_words).to eq('1 minute and 1 second ago')
+        expect((Time.now - 1*60*60 - 60).ago_in_words).to eq('1 hour and 1 minute ago')
+        expect((Time.now - 1*60*60*24 - 60*60).ago_in_words).to eq('1 day and 1 hour ago')
       end
 
       it "returns 1 second/minute/hour/day and 1 second/minute/hour/day ago ignoring third elements (residual irrelevant precisions)" do
-        (Time.now - 1*60*60 - 60 - 10).ago_in_words.should        == '1 hour and 1 minute ago'
-        (Time.now - 1*60*60*24 - 1*60*60 - 100).ago_in_words.should == '1 day and 1 hour ago'
-        (Time.now - 9*60*60*24 - 3*60*60 - 100).ago_in_words.should == '9 days and 3 hours ago'
+        expect((Time.now - 1*60*60 - 60 - 10).ago_in_words).to eq('1 hour and 1 minute ago')
+        expect((Time.now - 1*60*60*24 - 1*60*60 - 100).ago_in_words).to eq('1 day and 1 hour ago')
+        expect((Time.now - 9*60*60*24 - 3*60*60 - 100).ago_in_words).to eq('9 days and 3 hours ago')
       end
     end
   end
--- time_ago_in_words.gemspec
+++ time_ago_in_words.gemspec
@@ -24,12 +24,12 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
 
-  spec.add_development_dependency "bundler", ">= 1.2"
+  spec.add_development_dependency "bundler", ">= 1.3.5"
   spec.add_development_dependency "rake"
-  spec.add_development_dependency "rspec", "~> 2.13"
-  spec.add_development_dependency "redcarpet", ">= 2.2"
-  spec.add_development_dependency "yard", ">= 0.8.5.2"
-  spec.add_development_dependency "simplecov", ">= 0.7.1"
-  spec.add_development_dependency 'coveralls', '>= 0.5.8'
+  spec.add_development_dependency "rspec", "~> 2.14", ">= 2.14.1"
+  spec.add_development_dependency "redcarpet", ">= 3"
+  spec.add_development_dependency "yard", ">= 0.8.7.3"
+  spec.add_development_dependency "simplecov", ">= 0.8.2"
+  spec.add_development_dependency "coveralls", ">= 0.7.0"
 
 end
