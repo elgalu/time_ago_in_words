d6a7e71f82a925727576576c36ddea8488f3b970
Leo Gallucci <elgalu3@gmail.com>
2013-12-12 21:36:12 -0300

    RSpec fail fast. Require new improved RSpec expect() syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 ]
 SimpleCov.start
 
+# Internal
 require 'time_ago_in_words'
 
 # Require this file using `require "spec_helper"` within each of your specs
@@ -15,7 +16,8 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
-
-  # Run specs in random order to surface order dependencies.
-  config.order = 'random'
+  config.order = 'random' # Run specs in random order to surface order dependencies.
+  config.expect_with :rspec do |c|
+    c.syntax = :expect  # disable `should` syntax  http://goo.gl/BGxqP
+  end
 end
